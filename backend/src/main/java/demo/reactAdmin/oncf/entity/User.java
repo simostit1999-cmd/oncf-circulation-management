02ece4cfa6e62ba132855ab75edc010cc64b3b6d
simostit1999-cmd <simostit1999@gmail.com>
2026-03-06 22:33:10 +0000

    Add User entity

--- backend/src/main/java/demo/reactAdmin/oncf/entity/User.java
+++ backend/src/main/java/demo/reactAdmin/oncf/entity/User.java
@@ -1,1 +1,69 @@
+package demo.reactAdmin.oncf.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    private String nom;
+
+    private String prenom;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+}
