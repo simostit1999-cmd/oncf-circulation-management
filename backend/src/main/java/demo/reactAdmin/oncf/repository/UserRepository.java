c88e07466c06294829ce1ffdbff6bf7e6d5d6f38
simostit1999-cmd <simostit1999@gmail.com>
2026-03-06 22:35:06 +0000

    Add UserRepository

--- backend/src/main/java/demo/reactAdmin/oncf/repository/UserRepository.java
+++ backend/src/main/java/demo/reactAdmin/oncf/repository/UserRepository.java
@@ -1,1 +1,12 @@
+package demo.reactAdmin.oncf.repository;
 
+import demo.reactAdmin.oncf.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
